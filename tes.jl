3008a0f7b491a0079750415dd842737d91ea1709
gabrielbdsm <gabrielmartiliano2@gmail.com>
2021-09-11 06:28:11 -0300

    Update tes.jl

--- tes.jl
+++ tes.jl
@@ -1,9 +1,10 @@
 using Genie, Genie.Router, Genie.Renderer.Html, Genie.Requests
 
 
+include("t.jl")
 
 route("/") do
-    "Welcome to Genie!"
+    t.x()
   end
 
 up(parse(Int64, ENV["PORT"]), "0.0.0.0" ,async = false)
