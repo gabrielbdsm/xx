9b6cbdfb5f123e6c4b6b9d34ba5908679626d12b
gabrielbdsm <gabrielmartiliano2@gmail.com>
2021-09-10 21:18:31 -0300

    Update tes.jl

--- tes.jl
+++ tes.jl
@@ -1,7 +1,9 @@
 using Genie, Genie.Router, Genie.Renderer.Html, Genie.Requests
-using Merly
 
-webserverpath("/")
-@page "/" HTTP.Response(200,"Hello World!")
 
-start(host = "0.0.0.0", port =  parse(Int, ENV["PORT"])
+
+route("/") do
+    "Welcome to Genie!"
+  end
+
+up(async = false)
